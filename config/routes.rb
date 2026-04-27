a6fce8a5c5c952f95819564364a4030ccd3ab410
Gaby Vasconcelos <gvasconcelos@sagiss.com>
2026-04-27 08:55:20 -0500

    reset branch, updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if ENV.key?("PWP_PUBLIC_GATEWAY")
-    draw :public_users
-    draw :public_pushes
+  root "pushes#new"
 
-    # Add a route that handles the root path and returns a 404 error
-    root to: proc { |env| [404, {"Content-Type" => "text/html"}, ["<h1>Not Found</h1>"]] }
-  else
-    draw :admin
-    draw :madmin
-    draw :users
-    draw :pushes
-    draw :pwp_api
-
-    apipie
-    get "/help/api", to: "pages#api", as: :help_api
-
-    get "/pages/*id" => "pages#show", :as => :page, :format => false
-
-    mount Mailbin::Engine => :mailbin if Rails.env.development?
-
-    draw :redirects
-    draw :legacy_devise
-    draw :legacy_pages
-    draw :legacy_pushes
-
-    root to: "pushes#new"
-  end
+  resources :pushes, only: [:new, :create, :show]
 
   # Health check endpoint that returns a simple 200 OK response
   get "/up" => proc { |env|
-    [200, {"Content-Type" => "text/html"}, ["<html style='background:green;width:100%;height:100vh'></html>"]]
+    [200, {"Content-Type" => "text/html"}, ["OK"]]
   }
 
   post "/csp-violation-report", to: "csp_reports#create"
